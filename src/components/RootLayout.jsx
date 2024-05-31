6d8e8f5bafc5f0d33174db64a5018ec178648692
molero3111 <molero3111@gmail.com>
2024-05-31 15:02:41 -0400

    Removed comment from root layout

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -6,10 +6,6 @@ const RootLayout = () => {
     const navigation = useNavigation();
     return (
         <>
-            {/* {navigation.state === 'loading' && 
-            <Spinner animation="border" role="status" variant="dark">
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>} */}
             <NotesNavbar navigationState={navigation.state} />
             <main>
                 <Outlet />
