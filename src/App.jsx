6f1a79a13c48e5bc7f64debee18174ca5642c45f
molero3111 <molero3111@gmail.com>
2024-06-17 20:15:57 -0400

    Added lazy loading to note components

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,91 @@
-import './styles.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import "./styles.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./components/RootLayout";
-import Notes, { loadNotes, loadArchivedNotes } from "./components/Notes/Notes";
 import ErrorPage from "./components/ErrorPage";
 import Login from "./components/Login";
 import ProtectedComponent from "./components/ProtectedComponent";
 import SignUp from "./components/SignUp";
-import NoteForm, { loadNoteDetail, loadTagsAndCategories } from './components/Notes/NoteForm';
+import { loadNoteDetail } from "./components/Notes/NoteForm";
+import { lazy, Suspense } from "react";
 
-export const appURL = import.meta.env.VITE_APP_URL || '/notes-app/';
+export const appURL = import.meta.env.VITE_APP_URL || "/notes-app/";
 
-const router = createBrowserRouter([{
-  path: appURL,
-  element: <RootLayout />,
-  errorElement: <ErrorPage />,
-  children: [
-    { index: true, element: <Login /> },
-    { path: 'signup', element: <SignUp /> },
-    {
-      path: 'notes', element: <ProtectedComponent />,
-      children: [
-        { index: true, element: <Notes />, loader: loadNotes },
-        { path: 'archived', element: <Notes />, loader: loadArchivedNotes },
-        { path: 'new', element: <NoteForm />, loader: loadTagsAndCategories },
-        { path: ':noteId/edit', element: <NoteForm />, loader: loadNoteDetail },
-      ]
-    },
-  ]
-}]);
+// Lazy loading components
+const Notes = lazy(() => import("./components/Notes/Notes"));
+const NoteForm = lazy(() => import("./components/Notes/NoteForm"));
 
+const router = createBrowserRouter([
+  {
+    path: appURL,
+    element: <RootLayout />,
+    // errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Login /> },
+      { path: "signup", element: <SignUp /> },
+      {
+        path: "notes",
+        element: <ProtectedComponent />,
+        children: [
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <Notes />
+              </Suspense>
+            ),
+            loader: () => {
+              return import("./components/Notes/Notes").then((module) =>
+                module.loadNotes()
+              );
+            },
+          },
+          {
+            path: "archived",
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <Notes />
+              </Suspense>
+            ),
+            loader: () => {
+              return import("./components/Notes/Notes").then((module) =>
+                module.loadArchivedNotes()
+              );
+            },
+          },
+          {
+            path: "new",
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <NoteForm />
+              </Suspense>
+            ),
+            loader: () => {
+              return import("./components/Notes/NoteForm").then((module) =>
+                module.loadTagsAndCategories()
+              );
+            },
+          },
+          {
+            path: ":noteId/edit",
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <NoteForm />
+              </Suspense>
+            ),
+            loader: (meta) => {
+              return import("./components/Notes/NoteForm").then((module) =>
+                module.loadNoteDetail(meta)
+              );
+            },
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
